e14cb974658f8c586a04a8ea3b65b0bbfd261238
AzharLuthfi <125525250+AzharLuthfi@users.noreply.github.com>
2024-03-06 08:25:05 +0700

    Add files via upload

---
+++ pertemuan4/StrukturListTest1.java
@@ -0,0 +1,20 @@
+package pertemuan4;
+
+public class StrukturListTest1 {
+	
+	//LATIHAN 3
+	public static void main(String[] args) {
+		StrukturList List = new StrukturList();
+		
+		List.addTail(3);
+		List.addTail(4);
+		List.addMid(7, 2);
+		List.addMid(8, 2);
+		List.addHead(5);
+		
+		List.displayElement();
+		
+		
+	}
+
+}
